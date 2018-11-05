236d761f07a1b601a33c45800ee0598fb8d4b29a
lifeart <lifeart92@gmail.com>
2018-11-05 19:27:36 +0300

    typings update

--- addon/component-managers/hooks-react.ts
+++ addon/component-managers/hooks-react.ts
@@ -9,44 +9,87 @@ export interface ComponentManagerArgs {
   positional: any[];
 }
 
+interface IHookedComponentWrapper {
+	renderFn: Function,
+	renderTimer: any,
+	args: {
+		[key: string]: any
+	}
+	update(): void
+}
 
-var currentContext = null;
-var currentStateCall = 0;
-var currentEffectCall = 0;
-var contextStates = new WeakMap();
-var contextHooks = new WeakMap();
-var contextHooksCache = new WeakMap();
-var templateContexts = new WeakMap();
+var CURRENT_CONTEXT: IHookedComponentWrapper | null = null;
+var CURRENT_STATE_CALL = 0;
+var CURRENT_EFFECT_CALL = 0;
+var COMPONENTS_STATES = new WeakMap();
+var COMPONENTS_HOOKS = new WeakMap();
+var COMPONENTS_HOOKS_CACHE_KEYS = new WeakMap();
+var COMPONENTS_TEMPLATES_CONTEXTS = new WeakMap();
 
-
-function scheduleRerender(compnentContext) {
+function scheduleRerender(compnentContext: IHookedComponentWrapper) {
 	compnentContext.renderTimer = throttle(compnentContext, 'update', 16);
 }
 
-export function useState(value: any) {
-	let currentStates = contextStates.get(currentContext);
-	let callId  = currentStateCall;
+function compnentSetup(instance: IHookedComponentWrapper) {
+	COMPONENTS_STATES.set(instance, []);
+	COMPONENTS_HOOKS.set(instance, []);
+	COMPONENTS_HOOKS_CACHE_KEYS.set(instance, []);
+	COMPONENTS_TEMPLATES_CONTEXTS.set(instance, {});
+}
+
+function componentDestroy(instance: IHookedComponentWrapper) {
+	const effects = COMPONENTS_HOOKS.get(instance);
+	effects.forEach((destroyCb: any)=>{
+		if (typeof destroyCb === 'function') {
+			destroyCb();
+		}
+	});
+	cancel(instance.renderTimer);
+	COMPONENTS_HOOKS_CACHE_KEYS.delete(instance);
+	COMPONENTS_STATES.delete(instance);
+	COMPONENTS_HOOKS.delete(instance);
+	COMPONENTS_TEMPLATES_CONTEXTS.delete(instance);
+}
+
+function beforeRerenderSetup(instance: IHookedComponentWrapper) {
+	CURRENT_CONTEXT = instance;
+	CURRENT_STATE_CALL = 0;
+	CURRENT_EFFECT_CALL = 0;
+}
+function afterRerenderTeardown() {
+	CURRENT_CONTEXT = null;
+}
+
+export function useState(value: any): [any, Function] {
+	if (CURRENT_CONTEXT === null) {
+		throw new Error('Unable to find component context');
+	}
+	const currentStates = COMPONENTS_STATES.get(CURRENT_CONTEXT);
+	const callId  = CURRENT_STATE_CALL;
 	if (currentStates[callId] === undefined) {
 		currentStates.push(value);
 	}
-	currentStateCall++;
-	return [ currentStates[callId], function(newValue: any) {
+	CURRENT_STATE_CALL++;
+	return [ currentStates[callId], function(this: IHookedComponentWrapper, newValue: any) {
 		currentStates[callId] = newValue;
 		scheduleRerender(this);
-	}.bind(currentContext)];
+	}.bind(CURRENT_CONTEXT) ];
 }
 
 export function useEffect(cb: Function, cacheKeys = false) {
-	let cacheKey = cacheKeys !== false ? cacheKeys.toString(): false;
-	let currentHooks = contextHooks.get(currentContext);
-	let hooksCache = contextHooksCache.get(currentContext);
-	let callId  = currentEffectCall;
+	if (CURRENT_CONTEXT === null) {
+		throw new Error('Unable to find component context');
+	}
+	const cacheKey = cacheKeys !== false ? cacheKeys.toString(): false;
+	const currentHooks = COMPONENTS_HOOKS.get(CURRENT_CONTEXT);
+	const hooksCache = COMPONENTS_HOOKS_CACHE_KEYS.get(CURRENT_CONTEXT);
+	const callId  = CURRENT_EFFECT_CALL;
 
 	if (currentHooks.length - 1 < callId) {
 		currentHooks.push(cb());
 		hooksCache.push(cacheKey);
 	} else {
-		let mustIvalidateCache = cacheKey === false || (hooksCache[callId] !== false) && (cacheKey !== hooksCache[callId]);
+		const mustIvalidateCache = cacheKey === false || (hooksCache[callId] !== false) && (cacheKey !== hooksCache[callId]);
 		if (mustIvalidateCache) {
 			if (typeof currentHooks[callId] === 'function') {
 				currentHooks[callId]();
@@ -55,14 +98,14 @@ export function useEffect(cb: Function, cacheKeys = false) {
 			hooksCache[callId] = cacheKey;
 		}
 	}
-	currentEffectCall++;
+	CURRENT_EFFECT_CALL++;
 	return ;
 }
 
 
 export default class ReactHooksComponentManager {
   static create(attrs: any) {
-    let owner = getOwner(attrs);
+    const owner = getOwner(attrs);
     return new this(owner);
   }
   capabilities: any;
@@ -74,58 +117,41 @@ export default class ReactHooksComponentManager {
     });
   }
 
-  createCompnentContext(Klass: Function, args: any = {}) {
-	let context = {
+  createCompnentContext(Klass: Function, args: any = {}): IHookedComponentWrapper {
+	const context: IHookedComponentWrapper = {
 		renderFn: Klass,
 		args,
+		renderTimer: null,
 		update() {
-			currentContext = this;
-			currentStateCall = 0;
-			currentEffectCall = 0;
-			let currentTemplateObject = templateContexts.get(this);
-			let newResult = this.renderFn(this.args);
-			setProperties(currentTemplateObject, newResult);
-			currentContext = null;
+			beforeRerenderSetup(this);
+			setProperties(COMPONENTS_TEMPLATES_CONTEXTS.get(this), this.renderFn(this.args));
+			afterRerenderTeardown();
 		}
 	};
-	contextStates.set(context, []);
-	contextHooks.set(context, []);
-	contextHooksCache.set(context, []);
-	templateContexts.set(context, {});
+	compnentSetup(context);
 	return context;
   }
-  createComponent(Klass: Function, args: ComponentManagerArgs): any {
+
+  createComponent(Klass: Function, args: ComponentManagerArgs): IHookedComponentWrapper {
 	let instance = this.createCompnentContext(Klass, args.named);
 	setOwner(instance, getOwner(this));
-	currentContext = instance;
-	currentStateCall = 0;
-	currentEffectCall = 0;
-	templateContexts.set(instance, instance.renderFn(args.named));
-	currentContext = null;
-    return instance as any;
+	beforeRerenderSetup(instance);
+	COMPONENTS_TEMPLATES_CONTEXTS.set(instance, instance.renderFn(args.named));
+	afterRerenderTeardown();
+    return instance as IHookedComponentWrapper;
   }
 
-  updateComponent(instance: any, args: ComponentManagerArgs) {
+  updateComponent(instance: IHookedComponentWrapper, args: ComponentManagerArgs) {
 	instance.args = args.named;
 	instance.update()
   }
 
-  destroyComponent(component: any) {
-	cancel(component.renderTimer);
-	let effects = contextHooks.get(component);
-	effects.forEach((destroyCb: any)=>{
-		if (typeof destroyCb === 'function') {
-			destroyCb();
-		}
-	});
-	contextHooksCache.delete(component);
-	contextStates.delete(component);
-	templateContexts.delete(component);
-	component.renderFn = null;
+  destroyComponent(component: IHookedComponentWrapper) {
+	componentDestroy(component);
   }
 
   getContext(component: any) {
-	return templateContexts.get(component);
+	return COMPONENTS_TEMPLATES_CONTEXTS.get(component);
   }
 
   didCreateComponent() {
--- tests/dummy/app/components/conference-speakers-react.js
+++ tests/dummy/app/components/conference-speakers-react.js
@@ -1,27 +1,24 @@
 import { reactComponent, useEffect, useState } from "hooks-component";
 
-function ConferenceSpeakersReact(args) {
-	const [ speakers ] = useState(['Tom', 'Yehuda', 'Ed']);
-	let [ current, updateCurrent ] = useState(0);
-	if (args.current) {
-		current = args.current;
-	}
-	// currentlySpeaking
-	useEffect(() => {
-		console.log('dummy effect');
-	});
+	function ConferenceSpeakersReact() {
+		const [ speakers ] = useState(['Tom', 'Yehuda', 'Ed']);
+		let [ current, updateCurrent ] = useState(0);
 
-	const next = () => {
-		let nextSpeaker = current + 1;
-		updateCurrent(nextSpeaker);
-	}
+		useEffect(() => {
+			//console.log('dummy effect');
+		});
 
-	return {
-		currentlySpeaking: speakers[current],
-		moreSpeakers: (speakers.length - 1) > current,
-		current,
-		next, speakers
+		const next = () => {
+			let nextSpeaker = current + 1;
+			updateCurrent(nextSpeaker);
+		}
+
+		return {
+			currentlySpeaking: speakers[current],
+			moreSpeakers: (speakers.length - 1) > current,
+			current,
+			next, speakers
+		}
 	}
-}
 
 export default reactComponent(ConferenceSpeakersReact);
